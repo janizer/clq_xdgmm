855e8e5174e2f5f1cb6785c2d866d0baf050b168
janizer <m.rouble@gmail.com>
2018-02-02 10:17:56 -0500

    no more appending to csv files
    additional formatting, processing information

--- db_finding.py
+++ db_finding.py
@@ -6,11 +6,13 @@ import scipy
 import pandas
 import xdgmm
 from astropy.io import fits
+import csv
+import os
 
 import requests
 import ast
 
-def WISE(req, RA, DEC): #M: tbh I'm not sure what 'req' does?
+def WISE(req, RA, DEC): 
 
     #req.replace(' ','+')
     url = 'https://irsa.ipac.caltech.edu/cgi-bin/Gator/nph-query?spatial=cone&catalog=allwise_p3as_mep&objstr=' + RA + '+' + DEC + '&size=5&outfmt=1'
@@ -37,39 +39,58 @@ def WISE(req, RA, DEC): #M: tbh I'm not sure what 'req' does?
     w2sig = []
     for x in range(0, len(retCutLines)-1):
             retCutData = retCutLines[x].split()
-            #print(retCutData)
             mjd.append(retCutData[9])
             w1.append(retCutData[10])
             w1sig.append(retCutData[11])
             w2.append(retCutData[13])
             w2sig.append(retCutData[14])
 
-    print(mjd) #modified julian date
-    print(w1)
-    print(w1sig)
-    print(w2)
-    print(w2sig)
+    if(checkNull(w1) == 0) or (checkNull(w2) == 0): #there were no w1 or w2 values for this object
+        return
+
+    #print(mjd) #modified julian date
+    makeCSV(mjd, w1, w1sig, w2, w2sig, req)
+    #print(w1)
+    #makeCSV(w1, req)
+    #print(w1sig)
+    #makeCSV(w1sig, req)
+    #print(w2)
+    #makeCSV(w2, req)
+    #print(w2sig)
+    #makeCSV(w2sig, req)
 
     #for x in range(0, len(ret)):
     #    print("\n", ret[x])
 
 def dr14(): #raiding the quasar catalogue
-    cat = fits.open("DR14Q_v3_1.fits")
-    #for entry in len(cat[1].data['RA']): #get them all
-    for entry in range(20):
+    cat = fits.open("../DR14Q_v3_1.fits")
+    #for entry in len(cat[1].data['RA'])/100.0: #get them all
+    for entry in range(2000, 5000):
         RA = cat[1].data['RA'][entry]
         DEC = cat[1].data['DEC'][entry]
-        RA_DEC_str = "" + str(RA) + " " + str(DEC) #this is just parsing it
+        print(entry)
+
+        RA_DEC_str = "" + str(RA) + "_" + str(DEC) #this is just parsing it
         WISE(RA_DEC_str, str(RA), str(DEC)) 
+
+def makeCSV(data1, data2, data3, data4, data5, obj):
+    outfile = open('./csvfiles/'+obj+'.csv', 'w') #creates/opens the csv file for appending
+    wr = csv.writer(outfile)
+    wr.writerow(data1)
+    wr.writerow(data2)
+    wr.writerow(data3)
+    wr.writerow(data4)
+    wr.writerow(data5)
+    print("done file for obj " + obj)
+
+def checkNull(arr):
+    val = 0
+    for i in range(len(arr)):
+        if arr[i] != 'null':
+            val = val + 1 #keep track of how many non-zero entries we find
+    return val
 
 
 #WISE('294.1256623 -22.4012492')
 dr14()
 
-
-#wget command through cis module in python to run terminal commands to get data / query wget
-
-#next:
-##feed xdgmm
-###average each object's mjd, w1, w1sig, w2, w2sig?
-###
